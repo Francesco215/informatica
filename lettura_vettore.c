47952d251d06f62eb18f7fb8e7d150ab669444a2
Francesco Sacco <francesco215@live.it>
2017-07-26 17:32:16 +0200

    minor

--- lettura_vettore.c
+++ lettura_vettore.c
@@ -10,26 +10,26 @@ gli si fornisce un puntatore a cui viene fatto puntare il vettore della sequenza
 uno stop che termina la sequenza
 
 */
-int MkVector(int *V,int stop){
+static int *vettore;
+int MkVector(int stop){
 	static int passi=0;
 	int a,b,c;
 	scanf("%d",&a);
 	if(a==stop){
-		V=(int *)malloc((passi-1)*sizeof(int));
+		vettore=(int *)malloc((passi-1)*sizeof(int));
 		return 0;
 	}
 	b=a;
 	c=passi;
 	passi++;
-	MkVector(V,stop);
-	printf("%d,%d\n",c,b);
-	V[c]=b;
+	MkVector(stop);
+	vettore[c]=b;
 	if(c==0) return passi;
 	return 0;
 }
-int *vettore;
+
 int main(void){
-	int passi=MkVector(vettore,0);
+	int passi=MkVector(0);
 	for(int i=0;i<passi;i++) printf("%d  ",vettore[i]);
 	printf("\n");
 	return 0;
